6b331ee6fcc458f8290fffcee4af9c076d4cdc03
marionsy <marionantoniette.sy@gmail.com>
2022-09-28 14:25:44 -0700

    finished comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,40 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// Create a new comment
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const comment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete a comment
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const comment = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!comment) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
