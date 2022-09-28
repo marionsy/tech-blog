cce63461cd60d1fc39f3fc208a2feb8d995a11c2
marionsy <marionantoniette.sy@gmail.com>
2022-09-28 12:45:48 -0700

    finished homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,15 +2,11 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models'); 
 const withAuth = require('../utils/auth');
 
+// Get all posts for homepage
 router.get('/', async (req, res) => {
   try {
       const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
+        include: [{ model: User }],
       });
       const posts = postData.map((post) => post.get({ plain: true }));
 
@@ -22,3 +18,63 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Get a single post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User }, {model: Comment }],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+      res.render('singlepost', { 
+      ...posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get route to login
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+// Get route to signup
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
