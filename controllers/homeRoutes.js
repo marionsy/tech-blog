bcce048c736380fe9a5e0c3c8c4b040d81347fab
marionsy <marionantoniette.sy@gmail.com>
2022-09-28 11:37:55 -0700

    created index.js route, finished user route

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models'); 
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+      const postData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+      const posts = postData.map((post) => post.get({ plain: true }));
+
+      res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
